c37f13e6895dca0848ba494b4963b5b2fe32c0b0
Mihai Listov <mihailistov@gmail.com>
2017-09-21 20:36:23 -0400

    first commit

---
+++ Justo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Justo
+//
+//  Created by Mihai on 2017-09-21.
+//  Copyright © 2017 Mihai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
